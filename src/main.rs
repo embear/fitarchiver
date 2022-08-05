50084f25f1eb919e66f0326f4a11752130a1b150
Markus Braun <markus.braun@krawel.de>
2022-08-05 18:53:28 +0200

    feat: actually copy/move the files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::{DateTime, Local, TimeZone};
 use clap::{Arg, Command};
-use std::fs::File;
+use std::fs::{self, File};
 use std::process::ExitCode;
 
 struct ActivityData {
@@ -17,13 +17,13 @@ fn parse_fit_file(filename: &str) -> Result<ActivityData, String> {
     // open FIT file
     let mut fp = match File::open(filename) {
         Ok(fp) => fp,
-        Err(_err) => return Err(String::from("unable to open FIT file")),
+        Err(_err) => return Err(format!("Unable to open '{}'", filename)),
     };
 
     // parse FIT file to data structure
     let parsed_data = match fitparser::from_reader(&mut fp) {
         Ok(parsed_data) => parsed_data,
-        Err(_err) => return Err(String::from("unable to parse FIT file")),
+        Err(_err) => return Err(format!("Unable to parse '{}'", filename)),
     };
 
     // iterate over all data elements
@@ -38,7 +38,12 @@ fn parse_fit_file(filename: &str) -> Result<ActivityData, String> {
         .value()
     {
         fitparser::Value::Timestamp(val) => *val,
-        &_ => return Err(String::from("unexpected value in enum fitparser::Value")),
+        &_ => {
+            return Err(format!(
+                "Unexpected value in enum fitparser::Value in '{}'",
+                filename
+            ))
+        }
     };
     activity_data.sport = match parsed_data
         .iter()
@@ -51,7 +56,12 @@ fn parse_fit_file(filename: &str) -> Result<ActivityData, String> {
         .value()
     {
         fitparser::Value::String(val) => val.to_string(),
-        &_ => return Err(String::from("unexpected value in enum fitparser::Value")),
+        &_ => {
+            return Err(format!(
+                "Unexpected value in enum fitparser::Value in '{}'",
+                filename
+            ))
+        }
     };
 
     Ok(activity_data)
@@ -89,34 +99,93 @@ fn parse_arguments() -> clap::ArgMatches {
 }
 
 fn process_files(options: clap::ArgMatches) -> Result<String, String> {
-    let filenames: Vec<&str> = options.values_of("files").unwrap().collect();
-    println!("filenames: {}", filenames.join(" "));
-    let destination = options.value_of("directory").unwrap();
-    println!("directory: {}", destination);
-    println!("move: {:#?}", options.is_present("move"));
-
     println!(
         "Parsing FIT files using Profile version: {}",
         fitparser::profile::VERSION
     );
 
+    let mut file_counter: u16 = 0;
+    let mut error_counter: u16 = 0;
+
+    let destination = options.value_of("directory").unwrap();
+    let filenames: Vec<&str> = options.values_of("files").unwrap().collect();
+
     for filename in filenames {
         match parse_fit_file(filename) {
             Ok(val) => {
-                println!(
-                    "{} -> {}/{}/{}-{}.fit",
-                    filename,
-                    destination,
-                    val.timestamp.format("%Y/%m"),
+                let archive_path = format!("{}/{}", destination, val.timestamp.format("%Y/%m"));
+                let archive_file = format!(
+                    "{}/{}-{}.fit",
+                    archive_path,
                     val.timestamp.format("%Y-%m-%d-%H%M%S"),
                     val.sport
                 );
+
+                // Check if destination exists and is a directory, create it if needed
+                match fs::metadata(&archive_path) {
+                    Ok(val) => {
+                        if !val.is_dir() {
+                            return Err(format!(
+                                "'{}' exists but is not a directory",
+                                archive_path
+                            ));
+                        }
+                    }
+                    Err(_) => {
+                        print!("Creating directory '{}' ... ", archive_path);
+                        match fs::create_dir_all(&archive_path) {
+                            Ok(_) => println!("done"),
+                            Err(_) => {
+                                return Err(format!(
+                                    "Unable to create archive directory '{}'",
+                                    archive_path
+                                ))
+                            }
+                        }
+                    }
+                }
+
+                // Archiving files
+                if options.is_present("move") {
+                    print!("Moving '{}' -> '{}' ... ", filename, archive_file);
+                    match fs::copy(&filename, &archive_file) {
+                        Ok(_) => match fs::remove_file(&filename) {
+                            Ok(_) => {
+                                println!("done");
+                                file_counter += 1;
+                            }
+                            Err(_) => {
+                                println!("Unable to remove file '{}'", filename);
+                                error_counter += 1;
+                            }
+                        },
+                        Err(_) => {
+                            println!("Unable to create file '{}'", archive_file);
+                            error_counter += 1;
+                        }
+                    }
+                } else {
+                    print!("Copying '{}' -> '{}' ... ", filename, archive_file);
+                    match fs::copy(&filename, &archive_file) {
+                        Ok(_) => {
+                            println!("done");
+                            file_counter += 1;
+                        }
+                        Err(_) => {
+                            println!("Unable to create file '{}'", archive_file);
+                            error_counter += 1;
+                        }
+                    }
+                }
             }
-            Err(msg) => println!("ERROR: {}", msg),
+            Err(msg) => println!("{}", msg),
         };
     }
 
-    Ok(String::from("Processed XYZ files"))
+    Ok(format!(
+        "Processed {} files with {} errors",
+        file_counter, error_counter
+    ))
 }
 
 fn main() -> ExitCode {
